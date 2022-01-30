365ad090ffbf1d96eea4d8085a80a190d389c985
avzyr <avzyr@ya.ru>
2022-01-30 16:28:21 +0000

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RozetkaRul3',
+    version='0.1.0',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='az',
+    author_email='avzyr@ya.ru',
+    description='This script controls Broadlink\'s smart plugs'
+)
